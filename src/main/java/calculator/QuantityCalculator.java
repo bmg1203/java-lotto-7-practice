0040c97d846fb2ac181b84b72e4d5faaf3df839b
chan <ckstj8027@naver.com>
2024-11-04 23:07:33 +0900

    feat(calculator): 입력할수있는 가격범위 검증 추가

--- src/main/java/calculator/QuantityCalculator.java
+++ src/main/java/calculator/QuantityCalculator.java
@@ -4,25 +4,27 @@ import constant.ErrorConst;
 
 public class QuantityCalculator {
 
-    private static final int PRICE_UNIT = 1000;
+    private static final int PRICE_MIN_INPUT = 1000;
+    private static final long PRICE_MAX_INPUT = 4611686018000L ;
 
 
-    public static int calculateBuyQuantity(int buyPrice) {
-        isPositivePrice(buyPrice);
+    public static long calculateBuyQuantity(long buyPrice) {
+        checkPriceInputRange(buyPrice);
         return validateCanBeDivided(buyPrice);
     }
 
 
-    private static int validateCanBeDivided(int buyPrice) {
-        if (buyPrice % PRICE_UNIT != 0 ) {
+    private static long validateCanBeDivided(long buyPrice) {
+        if (buyPrice % PRICE_MIN_INPUT != 0 ) {
             throw new IllegalArgumentException(ErrorConst.ERROR_INVALID_AMOUNT);
         }
-        return buyPrice / PRICE_UNIT;
+        return buyPrice / PRICE_MIN_INPUT;
     }
 
-    private static boolean isPositivePrice(int buyPrice){
-        if(buyPrice <=0){
-            throw new IllegalArgumentException(ErrorConst.ERROR_NEGATIVE_AMOUNT );
+
+    private static boolean checkPriceInputRange(long buyPrice){
+        if(buyPrice <=0 ||buyPrice>PRICE_MAX_INPUT ){
+            throw new IllegalArgumentException(ErrorConst.ERROR_NOT_AVAILABLE_RANGE );
         }
         return true;
     }
