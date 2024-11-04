231a9093630f5e5ac105071098998e89cb028d68
chan <ckstj8027@naver.com>
2024-11-04 18:13:41 +0900

    feat(lotto): 검증 및 오름차순 정렬 기능추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,71 @@
 package lotto;
 
+import constant.ErrorConst;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+
+    private static final int LOTTO_NUMBER_COUNT_SIX = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
     private final List<Integer> numbers;
 
+
     public Lotto(List<Integer> numbers) {
+
+        this.numbers = new ArrayList<>(numbers);
         validate(numbers);
-        this.numbers = numbers;
+        sortNumbers();
     }
 
+
+    private void sortNumbers() {
+        Collections.sort(numbers);
+    }
+
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT_SIX) {
+            throw new IllegalArgumentException(ErrorConst.ERROR_INVALID_NUMBER_COUNT);
+        }
+
+    }
+
+
+    private void validateRange(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) > MAX_LOTTO_NUMBER || numbers.get(i) < MIN_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(ErrorConst.ERROR_NUMBER_OUT_OF_RANGE);
+            }
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    private void validateDuplicate(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            if (numbers.indexOf(number) != numbers.lastIndexOf(number)) {
+                throw new IllegalArgumentException(ErrorConst.ERROR_DUPLICATE_NUMBER);
+            }
+        }
+
+    }
+
+
+    public List<Integer> getLottoNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+
 }
