4fc6fe87e3f36e054d9deebadb8a47982d1ba334
chan <ckstj8027@naver.com>
2024-11-04 18:32:45 +0900

    feat(controller): Controller 도입

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,82 @@
+package controller;
+
+
+import lotto.Lotto;
+import lottoManager.LottoWinningResult;
+
+import service.LottoService;
+import view.InputView;
+import view.OutputView;
+import winningLottoNumber.WinningLottoNumber;
+
+public class Controller {
+
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+
+    public Controller(InputView inputView, OutputView outputView, LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+
+    public void play() {
+        int buyQuantity = getBuyQuantity();
+        outputView.showQuantityView(buyQuantity);
+
+        lottoService.buyLotto(buyQuantity);
+        outputView.showMyPurchasedLotto(lottoService.getPurchasedLotto());
+
+        Lotto createdLottoWithWinningNumber = getInputAndCreateLotto();   // 당첨번호의 로또 인풋으로부터 생성
+        createWinningLotto(createdLottoWithWinningNumber);
+
+
+        LottoWinningResult result = lottoService.getLottoResult();
+        outputView.displayFinalResult(result, buyQuantity);
+    }
+
+
+    private int getBuyQuantity() {
+        int buyPrice;
+        while (true) {
+            try {
+                buyPrice = inputView.startView();
+                return lottoService.calculateBuyQuantity(buyPrice);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    private Lotto getInputAndCreateLotto() {
+        String winningNumbersInput;
+        while (true) {
+            try {
+                winningNumbersInput = inputView.inputWinningNumberView();
+                return lottoService.createWinningLotto(winningNumbersInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    private WinningLottoNumber createWinningLotto(Lotto lottoWithWinningNumbers) {
+        String bonusNumberInput;
+        while (true) {
+            try {
+                bonusNumberInput = inputView.inputBonusNumberView();
+                return lottoService.createWinningLottoWithBonus(lottoWithWinningNumbers, bonusNumberInput);  //2
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+}
