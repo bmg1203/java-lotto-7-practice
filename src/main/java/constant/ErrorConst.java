f72d7ffdd29580b85c16c8f65eec78f3327f6c0e
chan <ckstj8027@naver.com>
2024-11-04 20:00:56 +0900

    feat(constant): 에러 상수 추가

--- src/main/java/constant/ErrorConst.java
+++ src/main/java/constant/ErrorConst.java
@@ -11,4 +11,6 @@ public class ErrorConst {
     public  static final String ERROR_INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
     public  static final String ERROR_NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호는 1 ~ 45 사이의 값이어야 합니다.";
     public  static final String ERROR_DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복되어서는 안됩니다.";
+
+    public  static final String ERROR_NEGATIVE_AMOUNT = "[ERROR] 금액은 양수를 입력해주세요.";
 }
