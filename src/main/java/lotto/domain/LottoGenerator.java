dfc045dca08d588821f08b45d7ba9ca71a683689
bmg1203 <bmg1203@naver.com>
2024-11-20 03:04:39 +0900

    [feat]: LottoGenerator 클래스 추가
    - 구입 금액에 따라 로또를 만들어줍니다.

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.constants.LottoNumbers;
+
+public class LottoGenerator {
+
+    private final List<Lotto> lottos;
+
+    public LottoGenerator(Purchase purchase) {
+        this.lottos = generateLottos(purchase);
+    }
+
+    private List<Lotto> generateLottos(Purchase purchase) {
+        Set<Lotto> noDuplicateLottos = new HashSet<>();
+
+        while (noDuplicateLottos.size() != purchase.purchaseCount()) {
+            Lotto lotto = generateRandomNumbers();
+            noDuplicateLottos.add(lotto);
+        }
+
+        return List.copyOf(noDuplicateLottos);
+    }
+
+    private Lotto generateRandomNumbers() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(LottoNumbers.LOTTO_MIN_RANGE.getNumber(),
+                LottoNumbers.LOTTO_MAX_RANGE.getNumber(),
+                LottoNumbers.LOTTO_LENGTH.getNumber()));
+    }
+}
