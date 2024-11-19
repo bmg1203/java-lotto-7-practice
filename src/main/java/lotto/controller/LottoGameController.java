caceb3a119c7078f3419192294601936c50ea676
bmg1203 <bmg1203@naver.com>
2024-11-20 04:38:43 +0900

    [feat]: LottoGameController 클래스 및 전체 실행 추가
    - LottoGameController에서 전체 프로그램을 실행합니다.
    - Application에서 컨트롤러를 실행합니다.

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,47 @@
+package lotto.controller;
+
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Purchase;
+import lotto.domain.WinningResult;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    private Purchase purchase;
+    private LottoGenerator lottoGenerator;
+    private Lotto winningNumber;
+    private Bonus bonus;
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        purchaseLottos();
+        setWinningNumber();
+        getResult();
+    }
+
+    private void purchaseLottos() {
+        purchase = inputView.purchaseInput();
+        lottoGenerator = new LottoGenerator(purchase);
+        outputView.purchasePrint(purchase, lottoGenerator);
+    }
+
+    private void setWinningNumber() {
+        winningNumber = inputView.winningNumberInput();
+        bonus = inputView.bonusInput(winningNumber);
+    }
+
+    private void getResult() {
+        WinningResult winningResult = new WinningResult(winningNumber, bonus, lottoGenerator, purchase);
+        outputView.resultPrint(winningResult);
+    }
+}
