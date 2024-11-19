d16fb1ab9297790e56d3c61717e220dae1fa94e3
bmg1203 <bmg1203@naver.com>
2024-11-20 02:49:23 +0900

    [feat]: Parser 문자열 리스트를 숫자 리스트롤 변경 메소드 추가
    - 문자열로 이루어진 리스트를 숫자로 만들어 반환합니다.
    - 숫자로 변경이 불가능하다면 오류를 발생시킵니다.

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,5 +1,7 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.constants.ErrorMessage;
 
 public class Parser {
@@ -7,6 +9,18 @@ public class Parser {
     public static int parseStringToInt(String input) {
         try {
             return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static List<Integer> parseStringListToIntList(List<String> inputs) {
+        try {
+            List<Integer> numbers = new ArrayList<>();
+            for (String input : inputs) {
+                numbers.add(Integer.parseInt(input));
+            }
+            return numbers;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.WRONG_INPUT_ERROR.getMessage());
         }
