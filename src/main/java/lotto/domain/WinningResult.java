71c1a35f7f943c9e01079224f5115e3b8a07be86
bmg1203 <bmg1203@naver.com>
2024-11-20 04:06:10 +0900

    [feat]: WinningResult 클래스 및 당첨 내역 구하기 추가
    - 당첨 번호와 보너스 번호를 이용해 등수별 당첨 내역을 구합니다.

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,77 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.constants.Prize;
+
+public class WinningResult {
+
+    private final Map<Integer, Integer> winningCount;
+    private final BigDecimal returnRate;
+
+    public WinningResult(Lotto winningNumber, Bonus bonus, LottoGenerator lottoGenerator, Purchase purchase) {
+        this.winningCount = getWinningCount(winningNumber, bonus, lottoGenerator);
+        this.returnRate = getReturnRate(purchase);
+    }
+
+    public Map<Integer, Integer> getWinningCount() {
+        return Collections.unmodifiableMap(winningCount);
+    }
+
+    public BigDecimal getReturnRate() {
+        return returnRate;
+    }
+
+    //당첨 개수 관련 메소드
+    private Map<Integer, Integer> getWinningCount(Lotto winningNumber, Bonus bonus, LottoGenerator lottoGenerator){
+        Map<Integer, Integer> resultSave = new HashMap<>();
+
+        for (Lotto lotto : lottoGenerator.getLottos()) {
+            boolean correctBonus = isBonus(lotto, bonus);
+            int correctCount = getCorrectCount(lotto, winningNumber);
+            getResult(resultSave, correctCount, correctBonus);
+        }
+
+        return resultSave;
+    }
+
+    private boolean isBonus(Lotto lotto, Bonus bonus) {
+        return lotto.getNumbers().contains(bonus.getBonus());
+    }
+
+    private int getCorrectCount(Lotto lotto, Lotto winningNumber) {
+        List<Integer> current = new ArrayList<>(List.copyOf(lotto.getNumbers()));
+        List<Integer> winning = List.copyOf(winningNumber.getNumbers());
+
+        current.retainAll(winning);
+        return current.size();
+    }
+
+    private void getResult(Map<Integer, Integer> resultSave, int correctCount, boolean correctBonus) {
+        Prize prize = findPrize(correctCount, correctBonus);
+
+        if (prize != null) {
+            resultSave.put(prize.getPlace(), resultSave.getOrDefault(prize.getPlace(), 0) + 1);
+        }
+    }
+
+    private Prize findPrize(int correctCount, boolean correctBonus) {
+        if (correctCount == Prize.SECOND.getMatchCount() && !correctBonus) {
+            return Prize.THIRD;
+        }
+
+        for (Prize prize : Prize.values()) {
+            if (prize.getMatchCount() == correctCount) {
+                return prize;
+            }
+        }
+        return null;
+    }
+
+    private BigDecimal getReturnRate(Purchase purchase) {
+    }
+}
