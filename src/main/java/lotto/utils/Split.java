d153b5ab7d939b287afe1f1d3dbdc29db9c7a00c
bmg1203 <bmg1203@naver.com>
2024-11-20 02:50:20 +0900

    [feat]: Split 클래스 및 쉽표(,) 문자열 분리 메소드 추가
    - 쉼표(,)로 문자열을 분리하여 문자열 리스트로 반환합니다.

---
+++ src/main/java/lotto/utils/Split.java
@@ -0,0 +1,13 @@
+package lotto.utils;
+
+import java.util.List;
+
+public class Split {
+
+    private static final String COMMA = ",";
+
+    public static List<String> commaSplit(String input) {
+        input = input.replaceAll(" ", "");
+        return List.of(input.split(COMMA));
+    }
+}
