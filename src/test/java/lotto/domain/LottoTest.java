218a96b4a9c587fa267713391c6b9b31458d72b2
bmg1203 <bmg1203@naver.com>
2024-11-20 05:22:56 +0900

    [test]: LottoTest 정상 동작 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import lotto.constants.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -37,5 +39,17 @@ class LottoTest {
     void 로또_숫자가_최대범위를_벗어난_예외() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 정상_동작_테스트() {
+        //given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        //when
+        Lotto lotto = new Lotto(numbers);
+
+        //then
+        assertThat(lotto.getNumbers().size()).isEqualTo(LottoNumbers.LOTTO_LENGTH.getNumber());
     }
 }
