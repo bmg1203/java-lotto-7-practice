9dec09149f06707610637c33fb653c0c06b5480b
bmg1203 <bmg1203@naver.com>
2024-11-20 02:39:23 +0900

    [feat]: ErrorMessage 구입 금액 최소 가격 오류 메시지 추가

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
 
     LOTTO_NUMBER_NOT_IN_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     PURCHASE_MONEY_ERROR("구입 금액은 1,000원 단위여야 합니다. "),
+    PURCHASE_MIN_MONEY_ERROR("구입 금액은 1,000원 이상이여야 합니다. "),
     DUPLICATE_LOTTO_NUMBER_ERROR("로또 번호는 중복되지 않아야 합니다. "),
     LOTTO_COUNT_ERROR("로또 번호는 6개 여야합니다. "),
     WRONG_INPUT_ERROR("잘못된 입력입니다. ");
