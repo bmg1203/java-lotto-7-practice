678d51ec9ad32f67f11e78ad3917264b7abe8f9d
bmg1203 <bmg1203@naver.com>
2024-11-20 02:15:04 +0900

    [feat]: Lotto 유효성 검사 추가
    - 중복 숫자 검사
    - 숫자 범위 검사

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,45 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constants.ErrorMessage;
+import lotto.constants.LottoNumbers;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LottoNumbers.LOTTO_LENGTH.getNumber()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_COUNT_ERROR.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> lottoSet = new HashSet<>(numbers);
+
+        if (lottoSet.size() != LottoNumbers.LOTTO_LENGTH.getNumber()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER_ERROR.getMessage());
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LottoNumbers.LOTTO_MIN_RANGE.getNumber() ||
+                number > LottoNumbers.LOTTO_MAX_RANGE.getNumber()) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_NOT_IN_RANGE_ERROR.getMessage());
+            }
+        }
+    }
 }
