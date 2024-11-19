199aa3a3a988a2a3bdf2871f7bb62c35db4aee88
bmg1203 <bmg1203@naver.com>
2024-11-20 02:30:58 +0900

    [feat]: Purchase 구입 금액 클래스 추가
    - 구입 금액를 담는 클래스 입니다.
    - 최소 가격 유효성 검사 메소드
    - 1,000원 단위 유효성 검사 메소드

---
+++ src/main/java/lotto/domain/Purchase.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import lotto.constants.ErrorMessage;
+
+public class Purchase {
+
+    private static final int MIN_MONEY = 1000;
+    private static final int DIVIDE_RESULT = 0;
+    private final int money;
+
+    public Purchase(int money) {
+        validateMin(money);
+        validateDivide(money);
+        this.money = money;
+    }
+
+    private void validateMin(int money) {
+        if (money < MIN_MONEY) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_MIN_MONEY_ERROR.getMessage());
+        }
+    }
+
+    private void validateDivide(int money) {
+        if (money % MIN_MONEY != DIVIDE_RESULT) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_MONEY_ERROR.getMessage());
+        }
+    }
+}
